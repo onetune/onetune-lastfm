1e92ebd335f6809eda6cf80823002242b9a45b68
Jonny Burger <jonathanburger11@gmail.com>
2015-05-05 17:44:50 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,4 @@
+# OneTune X Last.fm
+
+We use this code to allow users to scrobble to Last.fm.
+It is specific to onetune.fm, so you might not find it useful. Nonetheless, we want to open source more of our code.
---
+++ index.js
@@ -0,0 +1,62 @@
+var express = require('express');
+var LastFmNode = require('lastfm').LastFmNode;
+var _ = require('underscore');
+
+exports.LastFm = function(options) {
+	var lastfm = new LastFmNode({
+		api_key: options.api_key,
+		secret: options.secret,
+		useragent: 'OneTune.fm'
+	});
+
+	this.router = express.Router();
+
+	this.router.get('/auth', function (request, response) {
+		response.redirect('http://www.lastfm.de/api/auth?api_key=' + options.api_key + '&cb=http://' + request.headers.host + '/lastfm/callback');
+	});
+
+	this.router.get('/callback', function (request, response) {
+		if (!request.query.token) {
+			return response.end('Request must contain token.');
+		}
+		var session = lastfm.session({
+			token: request.query.token
+		})
+		session.on('success', function (session) {
+			console.log(session)
+			options.saveToken(request, session.key, session.user, function (success) {
+				if (!success) {
+					return response.end('You need to be logged in to link your account to Last.fm')
+				}
+				return response.redirect('http://' + request.headers.host + '/services');
+			});
+		});
+		session.on('error', function (error) {
+			return response.end('Authorization failed.');
+		})
+	});
+	this.router.post('/scrobble', function (request, response) {
+		var trackinfo = _.pick(request.body, 'artist', 'track', 'album', 'albumArtist');
+		trackinfo.timestamp = Math.ceil(Date.now() / 1000)
+		options.getToken(request, function (auth) {
+			if (!auth) {
+				return response.json({
+					success: false
+				})
+			}
+			var scrobble = lastfm.update('scrobble', lastfm.session({user: auth.service_username, key: auth.token}), trackinfo);
+			scrobble.on('success', function (msg) {
+				response.json({
+					success: true
+				});
+			})
+			scrobble.on('error', function (error) {
+				console.log(error)
+				response.json({
+					success: false
+				});
+			});
+		});
+	});
+	return this;
+}
